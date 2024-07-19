2aebca0d27b74ceb1f1f77b3d7564640b0591e0f
Aakib-Ali <aakibali449@gmail.com>
2024-07-19 20:20:05 +0530

    Do some changes

--- backend/routes/cart.Route.js
+++ backend/routes/cart.Route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addToCart, getCart, removeFromCart } from '../controllers/cartController.js';
+import { addToCart, getCart, removeFromCart } from '../controllers/cart.Controller.js';
 import authMiddleware from '../middleware/auth.js';
 
 const cartRouter = express.Router();
